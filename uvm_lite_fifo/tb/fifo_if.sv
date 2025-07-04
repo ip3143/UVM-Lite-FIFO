5aca810151dac22b52b2ab077c4de29ff35dddea
Ian Park <113217240+ip3143@users.noreply.github.com>
2025-07-03 21:54:52 -0700

    Delete uvm_lite_fifo directory, update with final fixed version

--- uvm_lite_fifo/tb/fifo_if.sv
+++
@@ -1,6 +0,0 @@
-interface fifo_if #(parameter WIDTH = 8) (input logic clk, reset);
-    logic wr_en, rd_en;
-    logic [WIDTH-1:0] data_in;
-    logic [WIDTH-1:0] data_out;
-    logic full, empty;
-endinterface
