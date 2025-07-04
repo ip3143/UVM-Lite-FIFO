f5b510844e06bc6197b5696e35ee87b01e6b287f
Ian Park <113217240+ip3143@users.noreply.github.com>
2025-07-03 18:29:04 -0700

    Delete uvm_lite_fifo/tb/fifo_transation.sv

--- uvm_lite_fifo/tb/fifo_transation.sv
+++
@@ -1,8 +0,0 @@
-class fifo_transaction;
-    rand bit write;
-    rand bit [7:0] data;
-
-    function string sprint();
-        return $sformatf("write=%0b data=0x%0h", write, data);
-    endfunction
-endclass
